29a99cbcb3611535df4fa9c40dc20e28c5cbf1e0
Mohsin-Ali92 <mohsin93790@gmail.com>
2021-05-10 13:36:09 +0530

    update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,6 @@ import Layout from "../components/layout"
 
 const IndexPage = ({ data }) => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <ul>
       {data.allStrapiArticle.edges.map(document => (
         <li key={document.node.id}>
@@ -21,8 +18,6 @@ const IndexPage = ({ data }) => (
         </li>
       ))}
     </ul>
-
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
